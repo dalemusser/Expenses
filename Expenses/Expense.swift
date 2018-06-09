93709c959887d0f2d02065072bbdbc850f8c9df9
Dale Musser <dale@dalemusser.com>
2018-06-08 17:47:04 -0700

    implemented expenses

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Dale Musser on 6/8/18.
+//  Copyright © 2018 Dale Musser. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
